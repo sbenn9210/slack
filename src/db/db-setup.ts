e60f3b9cda1108c700891cdf791334d9def50d43
Shawn <shawn.benny@umbrage.com>
2022-06-26 17:33:30 -0500

    Testing routes and db connection

--- src/db/db-setup.ts
+++ src/db/db-setup.ts
@@ -3,11 +3,10 @@ const knex = require("knex");
 const knexfile = require("./knexfile");
 const { Model } = require("objection");
 
-function setupDb() {
+export default function setupDb() {
   const db = knex(knexfile.development);
 
   // plug db config into objection
   Model.knex(db);
 }
 
-module.exports = setupDb;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 // https://dev.to/tylerlwsmith/using-a-typescript-interface-to-define-model-properties-in-objection-js-1231
 import express, { Express, Request, Response } from 'express'
+import User from './db/models/user.model'
+import setupDb from './db/db-setup'
+import bodyParser from 'body-parser';
 
+setupDb()
 
 const app: Express = express()
+
+app.use(bodyParser.json())
  
 app.get('/', (req: Request, res: Response) => {
     res.send('Response from the server')
 
+})
+
+app.post('/users', async (req: Request, res: Response) => {
+    const user: any = req.body
+    console.log(user)
+    const {name, username, email, password} = user;
+    const newUser = await User.query().insert({
+       name,
+       username,
+       email,
+       password
+    })
+
+    res.send(newUser)
 })
 
 app.listen(8080, () => {
