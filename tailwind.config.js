ccba807e29692d573fa1b86b13617ae6d0909419
Shawn Benny <sbenn9210@gmail.com>
2022-08-07 16:43:02 -0500

    Add app shell

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        "1/4": "1 1 25%",
+        "1/3": "1 1 33%",
+        "1/2": "1 1 50%",
+        "2/3": "1 1 67%",
+        "3/4": "1 1 75%",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
